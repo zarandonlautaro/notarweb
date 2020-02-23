1ad8ce68a6debef50c61958667ca25f084c1df0b
Bruno <digiorgiobruno92@gmail.com>
2020-02-23 16:46:38 -0300

    configuracion php mailer

--- mendumy/php/mail.php
+++ mendumy/php/mail.php
@@ -1,6 +1,7 @@
 <?php
 // Import PHPMailer classes into the global namespace
 // These must be at the top of your script, not inside a function
+//configuracion php mailer
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
