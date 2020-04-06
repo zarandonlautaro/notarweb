61ff302efcc0aa8e533728f487f2d38bf974f291
Brunoman92 <digiorgiobruno92@gmail.com>
2020-04-06 17:46:12 -0300

    moodifique carteles

--- mendumy/js/home.js
+++ mendumy/js/home.js
@@ -55,12 +55,12 @@ function carga(comprados) {
                         $('#fila' + fila).append(
 
                             '<div class="col-xl-4 mt-2">' +
-                            '<div class="  mendocard shadow-lg" style="width: 18rem;" curso=' + r['id'] +  '>' +
-                            '<img src="imgcourses/' + r['imgname']+'"'  +  'id=img' + r['id'] + '" class="mendocard-picture">' +
+                            '<div class="  mendocard shadow-lg" style="width: 18rem;" curso=' + r['id'] + '>' +
+                            '<img src="imgcourses/' + r['imgname'] + '"' + 'id=img' + r['id'] + '" class="mendocard-picture">' +
                             '<div class="">' +
                             '<h5 class="pt-2">' + r['name'] + '</h5>' +
                             '<p class="">' + r['description'] + '</p>' +
-                            '<button class="curso btn btn-block btn-info text-white" tipo=Ver curso=' + r['id'] + ' id=curso' + r['id'] +'> Ver </button>' +
+                            '<button class="curso btn btn-block btn-info text-white" tipo=Ver curso=' + r['id'] + ' id=curso' + r['id'] + '> Ver </button>' +
                             '</div>' +
                             '</div>' +
                             '</div>');
@@ -68,17 +68,17 @@ function carga(comprados) {
                         $('#fila' + fila).append(
 
                             '<div class="col-xl-4 mt-2">' +
-                            '<div class="mendocard shadow-lg " style="width: 18rem;" curso=' + r['id'] +  '>' +
-                            '<img src="imgcourses/' + r['imgname']+'"' +  'img='+r['imgname'] +'" class="mendocard-picture">' +
+                            '<div class="mendocard shadow-lg " style="width: 18rem;" curso=' + r['id'] + '>' +
+                            '<img src="imgcourses/' + r['imgname'] + '"' + 'img=' + r['imgname'] + '" class="mendocard-picture">' +
                             '<div class="">' +
                             '<h5 class="pt-2">' + r['name'] + '</h5>' +
                             '<p class="">' + r['description'] + '</p>' +
-                            '<button class="curso btn btn-block btn-success " tipo=Comprar curso=' + r['id'] +' id=curso' + r['id'] + '> Comprar </button>' +
+                            '<button class="curso btn btn-block btn-success " tipo=Comprar curso=' + r['id'] + ' id=curso' + r['id'] + '> Comprar </button>' +
                             '<p class="" id="pago' + r["preferenceid"] + '"></p>' +
                             '</div>' +
                             '</div>' +
                             '</div>');
-                          
+
                         /*var form = document.createElement("form");
                         form.method = "POST";
                         form.action = "/procesar-pago";
@@ -89,24 +89,24 @@ function carga(comprados) {
                         script.src = 'https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js';    // use this for linked script
                         script.setAttribute("data-preference-id", r['preferenceid']);
                         document.getElementById('form_pago_' + r['preferenceid']).appendChild(script);*/
-                       
+
                     }
 
 
 
 
                 });
-                
+
 
 
                 $(".curso").click(function () {
                     $(this).removeClass('btn-success');
                     $(this).addClass('btn-dark');
-                    
+
                     $('.modal-body').empty();
 
                     let id = $(this).attr("curso");
-                    let tipo=$(this).attr("tipo");
+                    let tipo = $(this).attr("tipo");
                     console.log(id);
                     //Comprobamos que tenga el curso comprado
 
@@ -119,7 +119,7 @@ function carga(comprados) {
                             $('#curso' + id).addClass('btn-dark');
                             $('#curso' + id).attr("disabled", "disabled");
                             $('#curso' + id).html('<span class="spinner-border spinner-border-sm" disabled></span>');
-            
+
                         },
                         error: function (error) { //Si ocurre un error en el ajax
                             //alert("Error, reintentar. "+error);
@@ -127,7 +127,7 @@ function carga(comprados) {
                             $('#curso' + id).addClass('btn-success');
                             $('#curso' + id).html(tipo);
                             $('#curso' + id).removeAttr("disabled");
-            
+
                         },
                         complete: function () { //Al terminar la peticion, sacamos la "carga" visual
                             $('#curso' + id).removeClass('btn-dark');
@@ -138,11 +138,11 @@ function carga(comprados) {
                         success: function (courses) {
                             console.log(courses);
                             let r = JSON.parse(courses);
-                            let rs=r[0];
+                            let rs = r[0];
                             console.log(rs[0]);
                             console.log(rs['videoname']);
 
-                            if (rs['bought']==true) {
+                            if (rs['bought'] == true) {
                                 console.log("cargó el curso");
                                 $('#pago').empty().append('<h2 class="display-5 alert alert-info"> <strong>La carga de cursos estará habilitada en breve</strong>. </br></br>¡Muchas gracias!</h2>');
                                 $('#exampleModal').modal('show');
@@ -151,44 +151,44 @@ function carga(comprados) {
                                 $('#video').attr('src', "coursesvideos/" + rs['videoname']);
                                 $('#modalvideo').modal("show");*/
 
-                            }else{
-                                console.log("Elemento no comprado");  
+                            } else {
+                                console.log("Elemento no comprado");
                                 console.log(rs["preferenceid"]);
-                                var preferenceid=rs["preferenceid"];
+                                var preferenceid = rs["preferenceid"];
                                 //var img=  $('#img'+id);
 
-                              
-                                if(preferenceid!=0){
-                                //creamos formulario de pago  
-                                $('#pago').empty();
-                                var form = document.createElement("form");
-                                form.method = "POST";
-                                form.action = "/procesar-pago";
-                                form.id = "form_pago";
-                                document.getElementById('pago').appendChild(form);
-                                var script = document.createElement("script");
-                                script.type = "text/javascript";
-                                script.src = 'https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js';    // use this for linked script
-                                script.setAttribute("data-preference-id", preferenceid);
-                                document.getElementById('form_pago').appendChild(script);
-                                }else{
-                                    var cartel=rs['cartel'];
+
+                                if (preferenceid != 0) {
+                                    //creamos formulario de pago  
+                                    $('#pago').empty();
+                                    var form = document.createElement("form");
+                                    form.method = "POST";
+                                    form.action = "/procesar-pago";
+                                    form.id = "form_pago";
+                                    document.getElementById('pago').appendChild(form);
+                                    var script = document.createElement("script");
+                                    script.type = "text/javascript";
+                                    script.src = 'https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js';    // use this for linked script
+                                    script.setAttribute("data-preference-id", preferenceid);
+                                    document.getElementById('form_pago').appendChild(script);
+                                } else {
+                                    var cartel = rs['cartel'];
                                     $('#pago').empty().append(cartel);
                                 }
-                                
-                                
+
+
                                 $('#exampleModal').modal('show');
                                 //cuando se cierra el modal recargamos los cursos
                                 $("#exampleModal").on("hidden.bs.modal", function () {
-                                    
-                                        $('#jumbotron').removeClass('d-none');
-                                        $('#jumbotron').addClass('d-block');
-                                       carga(false);
-                                 
-                                    
+
+                                    $('#jumbotron').removeClass('d-none');
+                                    $('#jumbotron').addClass('d-block');
+                                    carga(false);
+
+
                                 });
                             }
-        
+
                         }
                     });
                 });
@@ -209,47 +209,47 @@ function admin(ok) {
             url: "./php/admin.php",
             type: "post",
             data: ok,
-         
 
-            beforeSend: function() { //Previo a la peticion tenemos un cargando
-               
+
+            beforeSend: function () { //Previo a la peticion tenemos un cargando
+
                 $('#contenedor_home').empty();//vaciamos el contenedor en el cual van a cargarse los cursos
                 $('#carga_cursos').show("fast");//mostramos rapidamente los elementos que representan a los cursos
             },
-            error: function(error) { //Si ocurre un error en el ajax
+            error: function (error) { //Si ocurre un error en el ajax
                 //alert("Error, reintentar. "+error);
-               
+
             },
-            complete: function() { //Al terminar la peticion, sacamos la "carga" visual
-               
+            complete: function () { //Al terminar la peticion, sacamos la "carga" visual
+
             },
 
-            success: function(data) {
-               
+            success: function (data) {
+
                 $('#carga_cursos').hide("fast");//escondemos rapidamente los elementos que representan a los cursos
-               //console.log(data);
+                //console.log(data);
                 //$('#alert').addClass('alert-warning');
                 $('#contenedor_home').empty().append(data);
 
-                $.getScript("./js/admin.js", function(){ 
-                    
-          
+                $.getScript("./js/admin.js", function () {
+
+
                 });
 
             }
 
         });
-        
+
     }
 
 }
 
 $(document).ready(function () {
-   
+
     carga(false);
     $("#cursos").click(function () {
-         $('#jumbotron').removeClass('d-none');
-         $('#jumbotron').addClass('d-block');
+        $('#jumbotron').removeClass('d-none');
+        $('#jumbotron').addClass('d-block');
         carga(false);
     });
     $("#cursos_comprados").click(function () {
@@ -260,26 +260,28 @@ $(document).ready(function () {
     $("#adminpanel").click(function () {
         $('#jumbotron').removeClass('d-block');
         $('#jumbotron').addClass('d-none');
-       
+
     });
 
 
     $('#adminpanel').click(() => {
-            admin(true);
-            console.log("Admin");
-     });
-     $('#subirvideo').click(() => {
-        $('#pago').empty().append('<h2 class="display-5 alert alert-info"> <strong>La subida de videos estará habilitada en breve</strong>. </br></br>¡Muchas gracias!</h2>');
+        admin(true);
+        console.log("Admin");
+    });
+
+    $('#subirvideo').click(() => {
+        $('#pago').empty().append('<h2 class="alert alert-info"> <strong>La subida de videos estará habilitada en breve</strong>. </br></br>¡Muchas gracias!</h2>');
         $('#exampleModal').modal('show');
 
- });
- $('#modificarcurso').click(() => {
-    $('#pago').empty().append('<h2 class="display-5 alert alert-info"> <strong>La modificación de cursos estará habilitada en breve</strong>. </br></br>¡Muchas gracias!</h2>');
-    $('#exampleModal').modal('show');
-});
-   
+    });
+
+    $('#modificarcurso').click(() => {
+        $('#pago').empty().append('<h2 class=" alert alert-info"> <strong>La modificación de cursos estará habilitada en breve</strong>. </br></br>¡Muchas gracias!</h2>');
+        $('#exampleModal').modal('show');
+    });
 
 
 
-    
+
+
 });
