061696051ff24f5d49057f482e7bf7f2bd187796
Brunoman92 <digiorgiobruno92@gmail.com>
2020-05-08 12:00:28 -0300

    se iliminaron los videos subidos al servidor ahora se suben al servidor de vimeo

--- mendumy/php/upload-video.php
+++ mendumy/php/upload-video.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require_once('../php/funcs.php');
 include("mysqli.php");
 //print_r( $_FILES['imagen']['name']);
@@ -6,21 +9,25 @@ include("mysqli.php");
 print_r($_POST);}
 */
 //print_r($_POST);
-//print_r($files);
+//print_r($_FILES);
+//print_r($_POST);
+//$descripcion = trim(filter_var($_POST['descripcion'], FILTER_SANITIZE_STRING));
+//$Cmps = explode("**", $descripcion);
+//print_r($Cmps);
+//die;
 
-//
-if (isset($_FILES['imagen']) && isset($_FILES['video']) && isset($_POST['titulo']) && isset($_POST['tema']) && isset($_POST['descripcion']) && isset($_POST['curso'])) {
+if (isset($_POST['videoID']) && isset($_POST['titulo']) && isset($_POST['tema']) && isset($_POST['descripcion']) && isset($_POST['curso'])) {
+    $operacion = $_POST['operacion'];
     //Almacenamos en variables llegadas por métodos post
-    if($_POST['tema']==0||!(isset($_POST['tema']))){
+    if ($_POST['tema'] == 0 || !(isset($_POST['tema']))) {
         $msg[] = "Seleccione tema.";
         echo resultBlock($msg, 2);
         die;
     }
-    if ($_FILES['video']['error'] === 0 && $_FILES['imagen']['error'] === 0) {
-        $imagen = $_FILES['imagen'];
-        $video = $_FILES['video'];
-    } else {
-        $msg[] = "Obligatorio seleccionar archivo de video e imagen de portada";
+
+    if (strlen(trim($_POST['videoID'])) < 1 || !(isset($_POST['tema']))) {
+
+        $msg[] = "Introduzca el id del video,porfavor.";
         echo resultBlock($msg, 2);
         die;
     }
@@ -59,77 +66,60 @@ if (isset($_FILES['imagen']) && isset($_FILES['video']) && isset($_POST['titulo'
     $idtheme = trim(filter_var($_POST['tema'], FILTER_SANITIZE_STRING));
     $description = trim(filter_var($_POST['descripcion'], FILTER_SANITIZE_STRING));
     $idcourse = trim(filter_var($_POST['curso'], FILTER_SANITIZE_STRING));
+    $videoname = $_POST['videoID'];
 
     if (!($title || $idtheme || $description)) {
         $msg[] = "Caracter no permitido";
         echo resultBlock($msg, 2);
         die;
     }
+    //echo 'operacion seleccionada ' . $operacion;
+    //cargamos el formulario en la base de datos
 
 
-
-
-    $imageExtension = $imagen["type"];
-    $allowedfileExtensions = array('image/jpg', 'image/jpeg', 'image/gif', 'image/png');
-    //controlamos que el tipo de imagen ingresado sea un tipo permitido 
-    if (in_array($imageExtension, $allowedfileExtensions)) {
-        $videoExtension = $video["type"];
-        $allowedfileExtensions2 = array('video/mp4', 'video/wmv', 'video/flv', 'video/mov', 'video/avi');
-        //hacemos lo mismo con el video
-        if (in_array($videoExtension, $allowedfileExtensions2)) {
-            //creamos un nombre para el archivo codificando el nombre del directorio 
-            //temporario del archivo (este siempre es diferente)
-            $imgname = md5($imagen["tmp_name"]) . "." . extension($imageExtension);
-            $videoname = md5($video["tmp_name"]) . "." . extension($videoExtension);
-            $rutaImagen = "../imgcourses/" . $imgname;
-            $rutaVideo = "../coursesvideos/" . $videoname;
-            //cargamos el formulario en la base de datos
-
-            $sql1 = MySQLDB::getInstance()->query("INSERT INTO videoscourse (idcourse,name,title,uploaddate,idtheme,imgvideo,description) VALUES ('$idcourse','$videoname','$title',NOW(),'$idtheme','$imgname','$description' ) ");
-            $sqlid = MySQLDB::getInstance()->query("SELECT LAST_INSERT_ID() as id");
-            $id = $sqlid->fetch_assoc();
-            $idvideo = $id['id'];
-           
-            if ($sql1) {
-
-                if (isset($_FILES['file'])) {
-                    foreach ($files as $archivo) {
-    
-                        $name = $archivo['name'];
-                        $filename=$archivo['filename'];
-                        $sql2 = MySQLDB::getInstance()->query("INSERT INTO files (idvideo,name,uploaddate,idcourse,filename) VALUES ('$idvideo','$name',NOW(),'$idcourse','$filename') ");
-                        if($sql2){
-                            move_uploaded_file($archivo["tmp_name"], $archivo['path']);
-                          
-
-                        }
-                   
-                    }
-                }
-    
-                //movemos la imagen y el video desde su ubicación temporaria hasta los directorios imgcourses y coursesvideo respectivamente
-                move_uploaded_file($imagen["tmp_name"], $rutaImagen);
-                move_uploaded_file($video["tmp_name"], $rutaVideo);
-
-
-
-
-                $msg[] = "¡Curso subido con éxito!";
-                echo resultBlock($msg, 1);
-                die;
-            } else {
-                $msg[] = "¡Error al cargar a la base de datos!";
-                echo resultBlock($msg, 2);
-                die;
-            }
-        } else {
-            $msg[] = "extensión de video no valida </br> Formatos permitidos[mp4,wmv,flv,mov,avi]";
-            echo resultBlock($msg, 2);
-            die;
-        }
+    if ($operacion == 'insert') {
+        //opcion  insert
+        $sql1 = MySQLDB::getInstance()->query("INSERT INTO videoscourse (idcourse,name,title,uploaddate,modificationdate,idtheme,description) VALUES ('$idcourse','$videoname','$title',NOW(),NULL,'$idtheme','$description')");
+        $sqlid = MySQLDB::getInstance()->query("SELECT LAST_INSERT_ID() as id");
+        $id = $sqlid->fetch_assoc();
+        $idvideo = $id['id'];
     } else {
-        $msg[] = "Extensión de imagen no valida </br> Formatos permitidos[jpg,jpeg,gif,png]";
+        //opcion  update
+        $id = $_POST['id'];
+        $idvideo = $id;
+        $sql1 = MySQLDB::getInstance()->query("UPDATE videoscourse SET  idcourse='$idcourse',name='$videoname',title='$title',modificationdate=NOW(),idtheme='$idtheme',description='$description'WHERE id='$id'");
+    }
+
+
+    if ($sql1) {
+
+        if (isset($_FILES['file'])) {
+            foreach ($files as $archivo) {
+
+                $name = $archivo['name'];
+                $filename = $archivo['filename'];
+                $sql2 = MySQLDB::getInstance()->query("INSERT INTO files (idvideo,name,uploaddate,idcourse,filename) VALUES ('$idvideo','$name',NOW(),'$idcourse','$filename') ");
+                if ($sql2) {
+                    move_uploaded_file($archivo["tmp_name"], $archivo['path']);
+                }
+            }
+        }
+
+        //movemos la imagen y el video desde su ubicación temporaria hasta los directorios imgcourses y coursesvideo respectivamente
+        //move_uploaded_file($imagen["tmp_name"], $rutaImagen);
+        //move_uploaded_file($video["tmp_name"], $rutaVideo);
+
+
+
+
+        $msg[] = "¡Curso subido con éxito!";
+        echo resultBlock($msg, 1);
+        die;
+    } else {
+
+        $msg[] = "¡Error al cargar a la base de datos!";
         echo resultBlock($msg, 2);
+
         die;
     }
 
