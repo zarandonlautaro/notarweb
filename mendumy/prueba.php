3d21aa67a897d6fbf4aec74ff98a855071f3ecf5
Brunoman92 <digiorgiobruno92@gmail.com>
2020-09-03 14:42:33 -0300

    Se agregó actualizacion de base de datos y un nuevo framework para datatables

--- mendumy/prueba.php
+++ mendumy/prueba.php
@@ -1,123 +1,291 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Tutorial DataTables</title>
+
+  <!-- Bootstrap CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!-- CSS personalizado -->
+  <link href="css/style.css" rel="stylesheet" type="text/css">
+  <!--datables CSS básico-->
+  <link rel="stylesheet" type="text/css" href="vendor/DataTables/datatables.min.css" />
+  <!--datables estilo bootstrap 4 CSS-->
+  <link rel="stylesheet" type="text/css" href="vendor/DataTables/DataTables-1.10.21/css/dataTables.bootstrap4.min.css">
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
-    <table class="table table-responsive-sm ">
-        <thead class="thead-dark">
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Nombre</th>
-                <th colspan="2" scope="col" class="text-center">Opciones</th>
+  <header>
+    <h1 class="text-center text-light">Tutorial</h1>
+    <h2 class="text-center text-light">Cómo usar <span class="badge badge-danger">DATATABLES</span></h2>
+  </header>
+  <div style="height:50px"></div>
 
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <th scope="row">1</th>
-                <td>Contabilidad</td>
-                <td colspan="2">
+  <!--Ejemplo tabla con DataTables-->
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="table-responsive">
+          <table id="table" class="table table-striped table-bordered" style="width:100%">
+            <thead>
+              <tr>
+                <th>Video</th>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>DNI</th>
+                <th>Fecha</th>
+                <th>Fecha</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Tiger Nixon</td>
+                <td>Arquitecto</td>
+                <td>Edinburgh</td>
+                <td>61</td>
+                <td>2011/04/25</td>
+                <td>$320,800</td>
+              </tr>
+              <tr>
+                <td>Garrett Winters</td>
+                <td>Contador</td>
+                <td>Tokyo</td>
+                <td>63</td>
+                <td>2011/07/25</td>
+                <td>$170,750</td>
+              </tr>
+              <tr>
+                <td>Cedric Kelly</td>
+                <td>Senior Javascript Developer</td>
+                <td>Edinburgh</td>
+                <td>22</td>
+                <td>2012/03/29</td>
+                <td>$433,060</td>
+              </tr>
+              <tr>
+                <td>Airi Satou</td>
+                <td>Contador</td>
+                <td>Tokyo</td>
+                <td>33</td>
+                <td>2008/11/28</td>
+                <td>$162,700</td>
+              </tr>
+              <tr>
+                <td>Bradley Greer</td>
+                <td>Software Engineer</td>
+                <td>London</td>
+                <td>41</td>
+                <td>2012/10/13</td>
+                <td>$132,000</td>
+              </tr>
 
-                    <div class="row d-flex justify-content-around">
-                        <div class="col "> <button class="modificar-curso btn btn-dark">Modificar</button> </div>
-                        <div class="col "> <button class="modificar-videos btn btn-info" >Modificar Videos</button> </div>
-                        <div class="col"> <button class="eliminar-curso btn btn-danger">Eliminar</button></div>
-                    </div>
+              <tr>
+                <td>Bruno Nash</td>
+                <td>Software Engineer</td>
+                <td>London</td>
+                <td>38</td>
+                <td>2011/05/03</td>
+                <td>$163,500</td>
+              </tr>
+              <tr>
+                <td>Sakura Yamamoto</td>
+                <td>Support Engineer</td>
+                <td>Tokyo</td>
+                <td>37</td>
+                <td>2009/08/19</td>
+                <td>$139,575</td>
+              </tr>
+              <tr>
+                <td>Thor Walton</td>
+                <td>Developer</td>
+                <td>New York</td>
+                <td>61</td>
+                <td>2013/08/11</td>
+                <td>$98,540</td>
+              </tr>
+              <tr>
+                <td>Finn Camacho</td>
+                <td>Support Engineer</td>
+                <td>San Francisco</td>
+                <td>47</td>
+                <td>2009/07/07</td>
+                <td>$87,500</td>
+              </tr>
+              <tr>
+                <td>Serge Baldwin</td>
+                <td>Data Coordinator</td>
+                <td>Singapore</td>
+                <td>64</td>
+                <td>2012/04/09</td>
+                <td>$138,575</td>
+              </tr>
+              <tr>
+                <td>Zenaida Frank</td>
+                <td>Software Engineer</td>
+                <td>New York</td>
+                <td>63</td>
+                <td>2010/01/04</td>
+                <td>$125,250</td>
+              </tr>
+              <tr>
+                <td>Zorita Serrano</td>
+                <td>Software Engineer</td>
+                <td>San Francisco</td>
+                <td>56</td>
+                <td>2012/06/01</td>
+                <td>$115,000</td>
+              </tr>
+              <tr>
+                <td>Jennifer Acosta</td>
+                <td>Junior Javascript Developer</td>
+                <td>Edinburgh</td>
+                <td>43</td>
+                <td>2013/02/01</td>
+                <td>$75,650</td>
+              </tr>
+              <tr>
+                <td>Cara Stevens</td>
+                <td>Sales Assistant</td>
+                <td>New York</td>
+                <td>46</td>
+                <td>2011/12/06</td>
+                <td>$145,600</td>
+              </tr>
+              <tr>
+                <td>Hermione Butler</td>
+                <td>Regional Director</td>
+                <td>London</td>
+                <td>47</td>
+                <td>2011/03/21</td>
+                <td>$356,250</td>
+              </tr>
+              <tr>
+                <td>Lael Greer</td>
+                <td>Systems Administrator</td>
+                <td>London</td>
+                <td>21</td>
+                <td>2009/02/27</td>
+                <td>$103,500</td>
+              </tr>
+              <tr>
+                <td>Jonas Alexander</td>
+                <td>Developer</td>
+                <td>San Francisco</td>
+                <td>30</td>
+                <td>2010/07/14</td>
+                <td>$86,500</td>
+              </tr>
+              <tr>
+                <td>Shad Decker</td>
+                <td>Regional Director</td>
+                <td>Edinburgh</td>
+                <td>51</td>
+                <td>2008/11/13</td>
+                <td>$183,000</td>
+              </tr>
+              <tr>
+                <td>Michael Bruce</td>
+                <td>Javascript Developer</td>
+                <td>Singapore</td>
+                <td>29</td>
+                <td>2011/06/27</td>
+                <td>$183,000</td>
+              </tr>
+              <tr>
+                <td>Donna Snider</td>
+                <td>Customer Support</td>
+                <td>New York</td>
+                <td>27</td>
+                <td>2011/01/25</td>
+                <td>$112,000</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
 
-                </td>
 
-            </tr>
 
-        </tbody>
-    </table>
-<div class="container"></div>
+  <!--Ejemplo tabla con HTML-->
+  <!--<div style="height:50px"></div> 
+    <div class="container">
+    <h3>Tabla con <span class="badge badge-secondary">HTML puro</span></h3>
+    <div class="row">
+            <div class="col-lg-12">
+                <table border=1 cellpadding=1>
+                    <thead>
+                        <th>Nombre</th>
+                        <th>País</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Juan Perez</td>
+                            <td>Argentina</td>
+                        </tr>
+                        <tr>
+                            <td>María Pía</td>
+                            <td>Venezuela</td>
+                        </tr>
+                        <tr>
+                            <td>Juan Caros</td>
+                            <td>Perú</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+    </div>
+    </div>    -->
 
+
+  <!--Ejemplo tabla con bootstrap 4-->
+  <!--<div style="height:50px"></div>               
+    <div class="container">
+    <h3>Tabla con <span class="badge badge-secondary">BOOTSTRAP 4</span></h3>
+    <div class="row">
+            <div class="col-lg-12">                    
+                <table class="table table-striped table-hover table-bordered">
+                    <thead class="thead-dark">
+                        <th>Nombre</th>
+                        <th>País</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Juan Perez</td>
+                            <td>Argentina</td>
+                        </tr>
+                        <tr>
+                            <td>María Pía</td>
+                            <td>Venezuela</td>
+                        </tr>
+                        <tr>
+                            <td>Juan Caros</td>
+                            <td>Perú</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+    </div>
+    </div>-->
+
+  <!-- jQuery, Popper.js, Bootstrap JS -->
+
+  
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <!-- datatables JS -->
+  <script src="vendor/popper/popper.min.js"></script>
+  <script type="text/javascript" src="vendor/DataTables/datatables.min.js"></script>
+  <script src="vendor/DataTables/Buttons-1.6.3/js/dataTables.buttons.min.js"></script>
+  <script src="vendor/DataTables/JSZip-2.5.0/jszip.min.js"></script>
+  <script src="vendor/DataTables/Buttons-1.6.3/js/buttons.html5.min.js"></script>
+  <script type="text/javascript" src="js/datatable-lanzador.js"></script>
+ 
 
 </body>
-
-
-<script src="vendor/jquery/jquery.min.js"></script>
-
-
-<script type="text/javascript">
-    //funciones para inputs dinamicos----------------------------------------------------------------------------
-    function agregarinput() {
-        $('#file1').val("");
-        $('#btnDel').attr('disabled', 'disabled');
-        $('#btnAdd').click(function() {
-            var num = $('.clonedInput').length; // length devuelve la cantidad de elementos de una seleccion
-            var newNum = new Number(num + 1); // the numeric ID of the new input field being added
-
-            var newElem = '<fieldset id="input' + newNum + '" class="clonedInput custom-file">' +
-                '<label class="custom-file-label" for="file' + newNum + '">Seleccionar Archivo</label>' +
-                '<input type="file" class="custom-file-input " name="file' + newNum + '" id="file' + newNum + '" /></fieldset>';
-
-            $('#input' + num).after(newElem);
-            // enable the "remove" button
-            $('#btnDel').attr('disabled', false);
-
-            // business rule: you can only add 10 names
-            if (newNum == 10)
-                $('#btnAdd').attr('disabled', 'disabled');
-            nombreInputfile();
-
-            /*
-            // clonamos el elemento actual y le ponemos un id=id+numero de elemento
-            var newElem = $('#input' + num).clone().attr('id', 'input' + newNum);
-            // cambiamos el nombre del elemento
-            newElem.children(':last').attr('id', 'input' + newNum).attr('name', 'file' + newNum);
-            //newElem.children(':first').attr('placeholder', 'Nombre de archivo ' + newNum);
-            //$(this).siblings(".custom-file-label").addClass("selected").html(fileName);
-            */
-
-        });
-
-
-
-    }
-
-    function quitarinput() {
-
-        $('#btnDel').click(function() {
-            var num = $('.clonedInput').length; // how many "duplicatable" input fields we currently have
-            $('#input' + num).remove(); // remove the last element
-
-            // enable the "add" button
-            $('#btnAdd').attr('disabled', false);
-
-            // if only one element remains, disable the "remove" button
-            if (num - 1 == 1)
-                $('#btnDel').attr('disabled', 'disabled');
-        });
-
-
-    }
-
-    function nombreInputfile() {
-        $(".custom-file-input").on("change", function() {
-            var fileName = $(this).val().split("\\").pop();
-            console.log(fileName);
-            $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
-        });
-    }
-
-    $(document).ready(function() {
-
-        nombreInputfile();
-        agregarinput();
-        quitarinput();
-
-
-    });
-</script>
-
-<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="js/fontawesome-all.min.js"></script>
 
 </html>
